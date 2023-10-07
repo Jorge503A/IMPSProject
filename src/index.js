f8122b58f72a7c32b888cea2ad8923f79f75dc8d
Luis <ar20i04001@usonsonate.edu.sv>
2023-10-07 10:40:19 -0600

    Terminacion mantenimiento estudiantes carreras

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ app.set('views', path.join(__dirname, 'views')); // Configuracion de la ruta don
 app.engine('.hbs', exphbs.engine({
     defaultLayout: 'main', // Configuracion del layout principal
     layoutsDir: path.join(app.get('views'), 'layouts'), // Configuracion de la ruta de los layouts
-    extname: '.hbs' // Configura la extensión que tendran los archivos HandleBars
+    partialsDir: path.join(app.get('views'), 'partials'), // Configuracion de vistas parciales
+    extname: '.hbs', // Configura la extensión que tendran los archivos HandleBars
+    helpers: require('./lib/handlebars') // Configuracion de funciones
 }));
 
 app.set('view engine', '.hbs'); // Configuracion para ejecutar el motor de plantillas
